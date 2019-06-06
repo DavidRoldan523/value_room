da5d2211ebb24dc9791c66b6aefc80af573c4d83
cjgalvisc96 <cjgalvisc@unal.edu.co>
2019-06-06 12:09:56 -0500

    YouTube get reviews Embedded Ready

--- API/youtube/urls.py
+++ API/youtube/urls.py
@@ -1,23 +1,12 @@
 from django.conf.urls import url
-from .views import get_videos_comments, get_videos, get_videos_embedded
+from .views import get_videos
 
 urlpatterns = [
-    url(
-        r'videos/embedded/',
-        get_videos_embedded.get_videos_embedded,
-        name='get_videos_embedded'
-    )
-]
-"""
-    url(
-        r'videos/comments/',
-        get_videos_comments.get_videos_comments,
-        name='get_videos_comments'
-    ),
     url(
         r'videos/',
         get_videos.get_videos,
         name='get_videos'
-    ),
-"""
+    )
+]
+
 
