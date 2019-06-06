97bf4f597e52cccaa7954307e90f98f5f31bcb5b
david_roldan <jdavidroldan523@gmail.com>
2019-06-06 17:40:49 -0500

    Services IG, FB only function get_posts & fix date format on both

--- API/facebook/urls.py
+++ API/facebook/urls.py
@@ -1,21 +1,11 @@
 # FACEBOOK
 from django.conf.urls import url
-from .views import get_posts_comments, get_posts, get_posts_embedded
+from .views import get_posts
 
 urlpatterns = [
     url(
-        r'comments/',
-        get_posts_comments.get_posts_comments,
-        name='get_posts_comments'
-    ),
-    url(
         r'posts/',
-        get_posts.get_posts,
-        name='get_posts'
-    ),
-    url(
-        r'embedded/',
-        get_posts_embedded.get_posts_embedded,
-        name='get_post_embedded'
+        get_posts.get_comments,
+        name='get_comments'
     )
 ]
--- API/instagram/urls.py
+++ API/instagram/urls.py
@@ -1,20 +1,11 @@
 from django.conf.urls import url
-from .views import get_comments, get_posts, get_posts_embedded
+from .views import get_posts
 
 urlpatterns = [
     url(
         r'posts/',
-        get_posts.get_posts,
-        name='get_posts'
-    ),
-    url(
-        r'comments/',
-        get_comments.get_comments,
+        get_posts.get_comments,
         name='get_comments'
-    ),
-    url(
-        r'posts/embedded',
-        get_posts_embedded.get_comments,
-        name='get_posts_embedded'
     )
 ]
+    
