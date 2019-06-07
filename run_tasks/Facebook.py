797a36c004008a9fb135d1721a188f3a8b41cf21
david_roldan <jdavidroldan523@gmail.com>
2019-06-07 08:39:20 -0500

    IG, FB, YouTube version from OOP classes ready

--- run_tasks/Facebook.py
+++ run_tasks/Facebook.py
@@ -13,14 +13,18 @@ class Facebook:
         self.database = database
         self.database_table = database_table
 
-    def load_data(self):
+    def get_data(self):
         response = requests.post(f'{self.url}',
                                  data={'page_id': f'{self.page_id}',
                                        'fb_token': f'{self.fb_token}',
                                        'since': f'{self.since}',
                                        'until': f'{self.until}'},
                                  headers={'Authorization': f'Token {self.token}'})
+        return response.json()
+
+        
+    def load_data(self):
         database = boto3.resource(self.database)
         table = database.Table(self.database_table)
-        for post in response.json():
+        for post in self.get_data():
             table.put_item(Item=post)
