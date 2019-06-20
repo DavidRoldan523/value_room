ff12c390872cbfb2187e06e1d18b79e299d53305
cjgalvisc96 <cjgalvisc@unal.edu.co>
2019-06-20 17:04:35 -0500

    Json Final ready yo mount how Service

--- test_apis/sentiment_test/json_to_csv.py
+++ test_apis/sentiment_test/json_to_csv.py
@@ -2,7 +2,9 @@ import csv
 import json
 from datetime import date as python_date
 from dateutil.rrule import rrule, DAILY
-
+from classifier import *
+import re
+from stop_words import get_stop_words
 
 
 def read_json(path):
@@ -53,11 +55,29 @@ def transform_date(date):
     return response
 
 
+def clean_string(string):
+    stop_words = get_stop_words('spanish')
+    string = re.sub(r"\b[a-zA-Z]{1}\b", '', string)  # Remove Special Characters
+    for word in stop_words:
+        if re.findall(fr'\b({word})\b', string):
+            string = re.sub(fr"\b({word})\b", '', string)
+    return string
+
+
+def get_frequency_words(string):
+    string = clean_string(string)
+    words_list = string.split()
+    words_frequency = [words_list.count(word) for word in words_list]
+    response = sorted(set(zip(words_list, words_frequency)), key=lambda x: x[1], reverse=True)
+    return response
+
+
 if __name__ == '__main__':
     data_crude = read_json('instagram_post.json')[0]
     since = transform_date(data_crude['since'])
     until = transform_date(data_crude['until'])
-    
+
+    clf = SentimentClassifier()
     json_exit = []
     json_exit = [
                     {
@@ -80,8 +100,10 @@ if __name__ == '__main__':
         date_temp = day.strftime("%Y-%m-%d")
         result_total_temp = {
                         'date': date_temp,
-                        'comments': []
+                        'comments': [],
+                        'frecuency_words': []
                       }
+        total_coments_by_day = ''
         for result in data_crude['results']:
             post_id = result['post_id']
             post_name = result['post_name']
@@ -92,19 +114,32 @@ if __name__ == '__main__':
                                 }
             for comment in result['comments']:
                 if comment['created_time'] == date_temp:
+                    comment_text = comment['comment_text']
+                    polarity = clf.predict(comment_text)
                     comment_temp = {
-                                        'comment_text': comment['comment_text'],
-                                        'polarity': ''
+                                        'comment_text': comment_text,
+                                        'polarity': polarity
                                     }
                     result_post_temp['results'].append(comment_temp)
+                    total_coments_by_day += comment_text
 
             if len(result_post_temp['results']) > 0:
                 result_total_temp['comments'].append(result_post_temp)
+
+        if total_coments_by_day != '':
+            for frecuency_words in get_frequency_words(clean_string(total_coments_by_day))[:5]:
+                frecuency_words_temp = {
+                                            'word': frecuency_words[0],
+                                            'frecuency': frecuency_words[1]
+                                        }
+                result_total_temp['frecuency_words'].append(frecuency_words_temp)
 
         if len(result_total_temp['comments']) > 0:
             json_exit[0]['results'].append(result_total_temp)
 
     print(json.dumps(json_exit, indent=4))
+
+
 
 
 
