57543a19a8a1e6a6c7b8e2d15548b3fcaf912450
Roldan <jdavidroldan523@gmail.com>
2019-08-28 16:40:54 -0500

    Fix data response 0 on days without data & sentiment fix response empty {}

--- run_tasks_AWS/tools/date.py
+++ run_tasks_AWS/tools/date.py
@@ -6,5 +6,5 @@ class Date:
     def __init__(self):
         self.date_today = datetime.datetime.now()
         self.dates_facebook = {'until': str(self.date_today + datetime.timedelta(days=1)).split(' ')[0],
-                               'since':  str(self.date_today + datetime.timedelta(days=-2)).split(' ')[0]}
-        self.date_youtube = f'{str(self.date_today + dateutil.relativedelta.relativedelta(months=-3)).split(" ")[0]}T01:00:00-00:00'
+                               'since':  str(self.date_today + datetime.timedelta(days=-30)).split(' ')[0]}
+        self.date_youtube = f'{str(self.date_today + dateutil.relativedelta.relativedelta(months=-5)).split(" ")[0]}T01:00:00-00:00'
